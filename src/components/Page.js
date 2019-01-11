b1c05ea8e62233b171816d0ca38cbe9b2b8d56e6
Joshua Nelson <jonelson@atlassian.com>
2019-01-11 00:58:50 -0800

    Fix actions on mobile

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import { Link } from "@reach/router";
+
 import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
 
 const PageInner = styled.div`
   color: white;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 const PageHeader = styled.div`
   width: 100%;
   background: white;
   color: black;
-  text-align: right;
   padding: 8px;
   box-sizing: border-box;
-  position: relative;
 `;
 const PageImage = styled.div`
   width: 100%;
@@ -24,7 +24,6 @@ const PageImage = styled.div`
   background-image: ${props => `url('${props.background}')`};
   background-size: cover;
   background-position-x: 50%;
-  position: absolute;
   top: 0;
   left: 0;
 `;
@@ -38,7 +37,6 @@ const PageTitle = styled.div`
 `;
 const PageBody = styled.div`
   width: 100%;
-  position: relative;
   background: white;
   color: black;
   padding: 10px;
@@ -48,29 +46,31 @@ const PageBody = styled.div`
   }
 `;
 
+const ActionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 const action = `
-  background: white;
   color: black;
-  font-size: 2em;
-  position: absolute;
-  bottom: 1em;
   text-transform: uppercase;
-  padding: 8px;
   text-decoration: underline;
+  text-underline-position: under;
+  margin: 20px 10px 20px 0;
 `;
 
 const ViewMore = styled.a`
-  ${action} right: 0;
-  border: 4px solid black;
-  border-right: none;
+  ${action}
+  &:before {
+    content: "🚀";
+  }
 `;
 
 const Back = styled(Link)`
-  ${action} left: 0;
-  cursor: pointer;
-
-  border: 4px solid black;
-  border-left: none;
+  ${action}
+  &:before {
+    content: "⏪";
+  }
 `;
 
 class Page extends Component {
@@ -101,15 +101,19 @@ class Page extends Component {
         <PageImage background={thisPage.image.url} />
         <PageHeader>
           <PageTitle>{thisPage.title}</PageTitle>
+          <ActionContainer>
+            <Back to="/">Back</Back>
+            {thisPage.externalLink && (
+              <ViewMore target="_blank" href={thisPage.externalLink}>
+                View more
+              </ViewMore>
+            )}
+          </ActionContainer>
           <PageContent>{thisPage.description}</PageContent>
         </PageHeader>
         <PageBody>
           <ReactMarkdown source={thisPage.body} />
         </PageBody>
-        <Back to="/">Back</Back>
-        {thisPage.externalLink && (
-          <ViewMore href={thisPage.externalLink}>View more</ViewMore>
-        )}
       </PageInner>
     );
   }
